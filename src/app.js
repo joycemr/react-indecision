af7969345e6e1251b312db0f6927cfbad8c19333
Michael Joyce <michael.joyce@illiniarts.com>
2022-06-15 13:16:22 -0500

    app.js changes initial templates

--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
 console.log('app.js is running')
 
 // JSX - Javascript XML
-var template = <h1>This is JSX from app translated with babel</h1>
+var app = {
+    title: 'Indecision Application',
+    subTitle: 'This is a subtitle in a paragraph tag',
+    options: ['One', 'Two']
+}
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subTitle && <p>{app.subTitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+    </ol>
+    </div>
+)
+
+var user = {
+    name: 'Michael Joyce',
+    age: 56,
+    location: "O'Fallon, IL",
+}
+
+const getLocation = (location) => {
+    if (location) {
+        return <p>Location: {location}</p>
+    }
+}
+
+var template2 = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {user.age >= 18 && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+)
+
 var appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
