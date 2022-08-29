e5f58b504780a8dd43ea2a994159311d28b19dc9
Michael Joyce <michael.joyce@illiniarts.com>
2022-08-29 16:01:31 -0500

    Strip out experimental code

--- src/app.js
+++ src/app.js
@@ -30,14 +30,29 @@ const getLocation = (location) => {
     }
 }
 
+let count = 0
+const addOne = () => {
+    count++
+    console.log(`count: ${count}`)
+}
+const minusOne = () => {
+    count > 0 ? count-- : count = 0
+    console.log(`count: ${count}`)
+}
+const reset = () => {
+    count = 0
+    console.log(`count: ${count}`)
+}
+
 const template2 = (
     <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        {user.age >= 18 && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
+        <h1>Count: {count}</h1>
+        <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>Reset</button>
     </div>
 )
 
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot)
