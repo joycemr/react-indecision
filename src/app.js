e972be3a407433a0fce30571e2c1c235ba42dd3e
Michael Joyce <michael.joyce@illiniarts.com>
2022-08-30 08:23:50 -0500

    Add onMakeDecision

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = {
     options: []
 }
 
-const addOptions = (e) => {
+const onAddOptions = (e) => {
     e.preventDefault()
     const option = e.target.elements.option.value
     if (option) {
@@ -17,9 +17,15 @@ const addOptions = (e) => {
     }
 }
 
-const clearOptions = () => {
+const onClearOptions = () => {
     app.options = []
     renderApp()
+}
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length)
+    const option = app.options[randomNum]
+    alert(option)
 }
 
 const appRoot = document.getElementById('app')
@@ -30,16 +36,17 @@ const renderApp = () => {
             <h1>{app.title}</h1>
             {app.subTitle && <p>{app.subTitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-        {
-            app.options.map(option => <li key={option}>{option}</li>)
-        }
-        </ol>
-        <form onSubmit={addOptions}>
-            <input type="text" name="option"></input>
-            <button>Add Option</button>
-        </form>
-            <button onClick={clearOptions}>Clear Options</button>
+            <button onClick={onMakeDecision} disabled={app.options.length === 0}>What should I do?</button>
+            <ol>
+                {
+                    app.options.map(option => <li key={option}>{option}</li>)
+                }
+            </ol>
+            <form onSubmit={onAddOptions}>
+                <input type="text" name="option"></input>
+                <button>Add Option</button>
+            </form>
+            <button onClick={onClearOptions}>Clear Options</button>
         </div>
     )
     ReactDOM.render(template, appRoot)
