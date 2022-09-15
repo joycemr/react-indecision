427aa998a0ffcb2a6ebf23b5181842fa32dd209b
Michael Joyce <michael.joyce@illiniarts.com>
2022-09-15 08:13:27 -0500

    pass in the options array as a prop

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ class IndecisionApp extends React.Component {
         this.handlePick = this.handlePick.bind(this);
         this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: []
+            options: props.options
         }
     }
 
@@ -31,12 +31,11 @@ class IndecisionApp extends React.Component {
     }
 
     render() {
-        const title = 'Indecision';
         const subTitle = 'Put your life in the hands of a computer.';
 
         return (
             <div>
-                <Header title={title} subTitle={subTitle}/>
+                <Header subTitle={subTitle}/>
                 <Action
                     hasOptions={this.state.options.length > 0}
                     handlePick={this.handlePick}
@@ -53,13 +52,21 @@ class IndecisionApp extends React.Component {
     }
 }
 
+IndecisionApp.defaultProps = {
+    options: []
+}
+
 const Header = (props) => {
     return (
         <div id='header'>
             <h1>{props.title}</h1>
-            <h2>{props.subTitle}</h2>
+            {props.subTitle && <h2>{props.subTitle}</h2>}
         </div>
     );
+}
+
+Header.defaultProps = {
+    title: 'Indecision'
 }
 
 const Action = (props) => {
