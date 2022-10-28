5f9edd9af2dca5458bc0d32e5522319a21ceb9de
Michael Joyce <michael.joyce@illiniarts.com>
2022-10-28 13:37:09 -0500

    WIP styling

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,19 +2,24 @@ import React from "react";
 import Option from "./Option";
 
 const Options = (props) => (
-        <div id='options'>
-            <button onClick={props.handleDeleteOptions}>Remove All</button>
-            {props.options.length ===0 && <p>Please add an option to get started...</p>}
-            {
-                props.options.map(option => (
-                    <Option
-                        key={option}
-                        optionText={option}
-                        handleDeleteOption={props.handleDeleteOption}
-                    />
-                ))
-            }
-        </div>
-    );
+    <div id='options'>
+        <button
+            className="button button--link"
+            onClick={props.handleDeleteOptions}
+        >
+            Remove All
+        </button>
+        {props.options.length === 0 && <p>Please add an option to get started...</p>}
+        {
+            props.options.map(option => (
+                <Option
+                    key={option}
+                    optionText={option}
+                    handleDeleteOption={props.handleDeleteOption}
+                />
+            ))
+        }
+    </div>
+);
 
 export default Options;
